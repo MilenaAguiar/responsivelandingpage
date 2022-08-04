fb8b4e95c0639eb6af7366d2021152276b98ddf7
NayraCruz <nayra.ncruz@gmail.com>
2022-08-04 19:49:29 -0300

    Inclusão de scrim na modal

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-/* 
+/* -----------
     Modal
-*/
-const modal = document.querySelector('.modal')
+--------------*/
+const modal = document.querySelector('.modal-wrapper')
 const openModal = document.querySelector('.open-modal')
 const closeModal = document.querySelector ('.button')
 
@@ -13,9 +13,9 @@ closeModal.onclick = function () {
     modal.classList.remove('show');
 }
 
-/* 
+/* -----------
     Menu
-*/
+--------------*/
 const menuBotao = document.querySelector('.cabecalho-link')
 const menu = document.querySelector('.cabecalho-inside-menu')
 
